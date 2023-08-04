37d390761566dd48144904eb8ba605b258327828
JD <jann.driessen@gmx.de>
2023-08-04 21:59:44 +0200

    add big title component. testing chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import {
+  useDisclosure,
+  Button,
+  Flex,
+  Slide,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 
@@ -14,6 +22,9 @@ export function App() {
     <>
       <h1>OP Starter Project</h1>
 
+      <BigTitle />
+      <SlideEx />
+
       {/** @see https://www.rainbowkit.com/docs/connect-button */}
       <ConnectButton />
 
@@ -24,6 +35,37 @@ export function App() {
           <hr />
         </>
       )}
+    </>
+  );
+}
+
+function BigTitle() {
+  return (
+    <Flex alignItems={"center"} gap={4}>
+      <Box borderRadius={48} bg={"#FF0420"} w={9} h={9} />
+      <Text fontSize="5xl" fontWeight={400}>
+        Replay
+      </Text>
+    </Flex>
+  );
+}
+
+function SlideEx() {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onToggle}>Click Me</Button>
+      <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        ></Box>
+      </Slide>
     </>
   );
 }
